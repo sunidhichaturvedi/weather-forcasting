0305c6fded76431eeb25a2aa211169c2685fefad
Sunidhi Chaturvedi <sunidhichaturvedi258012@gmail.com>
2025-02-20 22:23:31 +0530

    Update javascipt.js

--- javascipt.js
+++ javascipt.js
@@ -1,11 +1,11 @@
  aync function weather(c) {
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=lucknow&appid=5def61d0bb6e971e8d1c3b912ce3cb05`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${c}&appid=5def61d0bb6e971e8d1c3b912ce3cb05`;
 let res= await fetch();
   console. log(res)
-  let data=res.json()
+  let data= await res.json()
   console. log(data)
-
-
+temp. innerHTML=math.round(data.main.temp);
+city.innerHTML=data.name;
 }
 
 var search = () => {
