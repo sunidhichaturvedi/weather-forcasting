da88f0075826c5265c703a2544337275b3804f63
Sunidhi Chaturvedi <sunidhichaturvedi258012@gmail.com>
2025-02-21 16:34:19 +0530

    Update javascipt.js

--- javascipt.js
+++ javascipt.js
@@ -4,8 +4,20 @@ let res= await fetch();
   console. log(res)
   let data= await res.json()
   console. log(data)
+  if(data.cod=='404')
+  {
+window. alert("enter again");
+   document. querySelector("search>input").value="";
+main.style.display="none";
+  }
+  else{
+   
 temp. innerHTML=math.round(data.main.temp);
 city.innerHTML=data.name;
+  wind.innerHTML=data.wind. speed+"km/hr";
+  humidity. innerHTML=data. main.humidity+"%";
+   main.style. display="block";
+  }
 }
 
 var search = () => {
